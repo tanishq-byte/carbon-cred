eea7fc4397ef1f38ec649386967c979bb7dc6b0d
Your Name <you@example.com>
2025-07-29 20:33:48 +0530

    fix: added build outDir for Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // ✅ This tells Vercel where to look
   },
 })
